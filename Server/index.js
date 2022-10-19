5c1e7063c3afdff5a5724695ad6445fc0370ddc6
PandaGamerPlayzz <zacharymiller515@gmail.com>
2022-10-19 16:09:59 -0400

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+const http = require("http");
+
+// PORT
+const PORT = 5000;
+
+// server create
+const server = http.createServer((req, res) => {
+   if (req.url === "/" || req.url === "/home") {
+      res.write("This is home page.");
+      res.end();
+   } else {
+      res.write("Not Found!");
+      res.end();
+   }
+});
+
+// server listen port
+server.listen(PORT);
+
+console.log(`Server is running on PORT: ${PORT}`);

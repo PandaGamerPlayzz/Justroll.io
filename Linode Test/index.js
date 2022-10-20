dc7c3a613aaa95e2ff696796d6f39382d9815bee
PandaGamerPlayzz <zacharymiller515@gmail.com>
2022-10-19 21:36:00 -0400

    Testing express app

--- Linode Test/index.js
+++ Linode Test/index.js
@@ -3,25 +3,38 @@ dotenv.config();
 
 const express = require('express');
 
+const PORT = process.env.PORT || 5000;
+const WEB_PATH = __dirname + (process.env.WEB_PATH || '/web/')
+
 const app = express();
 
-app.get('/endpoint-1', (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    data: {
-      message: 'Hello from endpoint 1',
-    },
-  });
+app.get('/', (req, res, next) => {
+    res.sendFile(WEB_PATH + 'index.html')
 });
 
-app.get('/endpoint-2', (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    data: {
-      message: 'Hello from endpoint 2',
-    },
-  });
+app.get('*', function(req, res, next) {
+    var err = new Error();
+    err.status = 404;
+    next(err);
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is running on this port ${PORT}`));
+app.use(function(err, req, res, next) {
+    if (err.status === 404) {
+        res.status(200).json({
+            error: 404
+        })
+    } else {
+        return next();
+    }
+});
+
+app.listen(PORT, () => console.log(`Server is running port ${PORT}`));
+
+// app.get('/endpoint-1', (req, res, next) => {
+//   res.status(200).json({
+//     success: true,
+//     data: {
+//       message: 'Hello from endpoint 1',
+//     },
+//   });
+// });
